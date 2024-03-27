3404ef1c536b31de1f3c90f92e45b5fe3d9c9dba
Ahmad Faishal Akbar <113009001+ahmadfaishal9611@users.noreply.github.com>
2024-03-27 10:16:41 +0700

    Update BalancePerCustomer.sql

--- BalancePerCustomer.sql
+++ BalancePerCustomer.sql
@@ -7,38 +7,25 @@ BEGIN
     WITH a AS
     (
         SELECT
-            trans.TransactionID,
             cus.CustomerName,
-            trans.TransactionDate,
             acc.AccountType,
             acc.Balance,
-            trans.Amount,
-            trans.TransactionType
+		SUM(CASE WHEN trans.TransactionType = 'Deposit' THEN trans.Amount ELSE ((-1) * trans.Amount) END) AS TotalAmount
         FROM 
             FactTransaction AS trans
         LEFT JOIN 
             DimAccount AS acc ON trans.AccountID = acc.AccountID
         LEFT JOIN 
             DimCustomer AS cus ON acc.CustomerID = cus.CustomerID
-		WHERE acc.Status = 'active'
-    ),
-    b AS
-    (
-        SELECT
-            CustomerName,
-            AccountType,
-            Balance,
-            SUM(CASE WHEN TransactionType = 'Deposit' THEN Amount ELSE 0 END) AS TotalDeposit,
-            SUM(CASE WHEN TransactionType != 'Deposit' THEN Amount ELSE 0 END) AS TotalNonDeposit
-        FROM a
-        GROUP BY CustomerName, AccountType, Balance
+	WHERE acc.Status = 'active'
+	GROUP BY cus.CustomerName, acc.AccountType, acc.Balance
     )
     SELECT
         CustomerName,
         AccountType,
         Balance,
-        (Balance + TotalDeposit - TotalNonDeposit) AS CurrentBalance
-    FROM b
+        (Balance + TotalAmount) AS CurrentBalance
+    FROM a
     WHERE CustomerName LIKE '%' + @name + '%';
 END;
 
